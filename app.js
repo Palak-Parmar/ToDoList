37a8a137522b443c5a8af1e7786171af43219dcb
Palak-Parmar <palak7parmar@gmail.com>
2021-03-04 18:06:27 +0530

    day

--- app.js
+++ app.js
@@ -4,41 +4,30 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
 var today= new Date();
-var currentday = today.getDay();
-var day="";
-
-switch(currentday)
+var options=
 {
-case 0:
-   day="Sunday";
-break;
-case 1:
-   day="Monday";
-break;
-case 2:
-   day="Tueday";
-break;
-case 3:
-   day="Wedneday";
-break;
-case 4:
-   day="Thursday";
-break;
-case 5:
-   day="Friday";
-break;
-case 6:
-   day="Saturday";
-break;
-}
-console.log("error");
+  weekday :"long",
+  year:"numeric",
+  month :"long",
+  day:"numeric"
+};
+
+var day = today.toLocaleDateString("en-US",options);
+
 
 res.render("list", {KindOfDay:day});
 });
+
+app.post("/",function(req,res)
+{
+  console.log(req.body.newval);
+});
+
 
 app.listen(3000, function(){
   console.log("Server started on port 3000.");
